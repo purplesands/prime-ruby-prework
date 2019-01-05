57799d22fda02ec6f5f109d6b824dca14dcbbb3d
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 22:46:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,5 +10,3 @@ end
 true
 end
 
-
-# To prove whether a number is a prime number, first try dividing it by 2, and see if you get a whole number. If you do, it can't be a prime number. If you don't get a whole number, next try dividing it by prime numbers: 3, 5, 7, 11 (9 is divisible by 3) and so on, always dividing by a prime number (see table below).
